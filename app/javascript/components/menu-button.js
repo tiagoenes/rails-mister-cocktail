f7a5fc0f65e35eff96e9ed425ccfdb661698c8d3
Tiago Enes <enes.tiago@gmail.com>
2020-11-20 16:24:34 +0100

    update menu space

--- app/javascript/components/menu-button.js
+++ app/javascript/components/menu-button.js
@@ -1,15 +1,12 @@
 const clickMenu = () => {
-const bt  = document.querySelector('.navbar-toggler');
+const bt  = document.querySelector('.navbar-collapse');
 
 const test = bt.addEventListener('click', (e) => {
   const list = document.querySelector('.navbar-space-logo');
-  const listItem = document.querySelector('.navbar-space-logo>li');
-  if (bt.getAttribute('class').includes('collapsed')) {
-    list.classList.remove("add-margin");
-    list.classList.remove("add-justify");
-  }else{
+  if (bt.getAttribute('class').includes('show')) {
     list.classList.add("add-margin");
-    list.classList.add("add-justify");
+  }else{
+    list.classList.remove("add-margin");
   }
 });
 
