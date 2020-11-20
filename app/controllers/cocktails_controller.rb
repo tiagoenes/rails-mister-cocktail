1f019e817016113ad34940f781f71546ddebb517
Tiago Enes <enes.tiago@gmail.com>
2020-11-20 10:44:20 +0100

    update logo

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,11 +2,19 @@ class CocktailsController < ApplicationController
   def index
     @new_cocktail = Cocktail.new
     @cocktails = Cocktail.all
+    @query = ""
   end
   def show
     @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
     @doses = Dose.where("cocktail_id = ?",params[:id])
+  end
+
+  def search
+    @query = params[:search].downcase
+    @new_cocktail = Cocktail.new
+    @cocktails = Cocktail.where("lower(name) like ?", "%#{@query }%")
+    render :index
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home', as: :home
   resources :cocktails do
+    collection do
+      post :search
+    end
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: :destroy
